103de95b7c9bfe41b4b73cdddc00372d3e5fc27c
olena <olena.krysan@gmail.com>
2024-12-12 14:02:15 +0200

    Day 11

--- Days/Day11.cs
+++ Days/Day11.cs
@@ -2,24 +2,73 @@
 
 namespace AdventOfCode2024.Days
 {
-    internal class Day11 : IDay<int>
+    internal class Day11 : IDay<long>
     {
-        public async Task<int> SolvePart1Async()
+        private Dictionary<string, long> _stones;
+        public async Task<long> SolvePart1Async()
+        {
+            return await Solve(25);
+        }
+        public async Task<long> Solve(int blinks)
         {
             await ReadInput();
-            return 0;
+            for (long i = 0; i < blinks; i++)
+            {
+                var newStones = new Dictionary<string, long>();
+                foreach (var stone in _stones)
+                {
+                    if (stone.Key == "0")
+                    {
+                        AddStone(newStones, "1", stone.Value);
+                    }
+                    else if (stone.Key.Length % 2 == 0)
+                    {
+                        var originalStone = stone.Key;
+                        var firstHalf = new string(originalStone.Substring(0, originalStone.Length / 2).SkipWhile(c => c == '0').ToArray());
+                        var secondHalf = new string(originalStone.Substring(originalStone.Length / 2).SkipWhile(c => c == '0').ToArray());
+                        firstHalf = firstHalf == "" ? "0" : firstHalf;
+                        secondHalf = secondHalf == "" ? "0" : secondHalf;
+                        AddStone(newStones, firstHalf, stone.Value);
+                        AddStone(newStones, secondHalf, stone.Value);
+                    }
+                    else
+                    {
+                        var number = long.Parse(stone.Key);
+                        var newKey = "" + number * 2024;
+                        AddStone(newStones, newKey, stone.Value);
+                    }
+                }
+                _stones = newStones;
+            }
+            return _stones.Sum(stone => stone.Value);
         }
 
-        public async Task<int> SolvePart2Async()
+        private void AddStone(Dictionary<string,long> stones, string key, long value)
         {
-            await ReadInput();
-            return 0;
+            if (stones.ContainsKey(key))
+            {
+                stones[key] += value;
+            }
+            else
+            {
+                stones.Add(key, value);
+            }
+        }
+
+        public async Task<long> SolvePart2Async()
+        {
+            return await Solve(75);
         }
 
         private async Task ReadInput()
         {
-            var input = ReadFileUtils.ReadTestFile(11);
-            //var input = await ReadFileUtils.ReadFileAsync(11);
+            var input = await ReadFileUtils.ReadFileAsync(11);
+            _stones = new Dictionary<string, long>();
+            var inputSplited = input[0].Split(' ');
+            foreach (var item in inputSplited)
+            {
+                _stones.Add(item, 1);
+            }
         }
     }
 }
